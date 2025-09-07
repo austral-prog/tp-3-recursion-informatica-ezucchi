4392a745b921207005989713fe99b1363462485d
Eugenio Zucchi <ez.zucchieugenio@gmail.com>
2025-09-07 17:10:05 -0300

    -m First hand-in

--- src/main/java/com/recursion/RecursiveFunctions.java
+++ src/main/java/com/recursion/RecursiveFunctions.java
@@ -5,43 +5,100 @@ import java.util.List;
 public class RecursiveFunctions {
 
     public static int recursiveIndexOf(List<String> list, String target) {
+        int recursiveInt = 0;
+        if (!list.isEmpty()) {
+            if (list.get(0).equals(target)) {
+                return 0;
+            }
+            recursiveInt = recursiveIndexOf(list.subList(1, list.size()), target);
+            if (recursiveInt == -1) {
+                return -1;
+            } else{
+                return recursiveInt + 1;
+            }
+        }
         return -1;
     }
 
     public static int recursiveIndexOfByIndex(List<String> list, String target, int index) {
-        return -1;
+        return recursiveIndexOf(list, target);
     }
 
     public static int recursiveIndexOfEmpty(List<String> list) {
+        int recursiveInt = 0;
+        if (!list.isEmpty()) {
+            if (list.get(0) == "") {
+                return 0;
+            }
+            recursiveInt = recursiveIndexOfEmpty(list.subList(1, list.size()));
+            if (recursiveInt == -1) {
+                return -1;
+            } else{
+                return recursiveInt + 1;
+            }
+        }
         return -1;
     }
 
     public static int recursivePut(String target, List<String> list) {
-        return -1;
+        int recursiveInt = recursiveIndexOfEmpty(list);
+        if (recursiveInt == -1) {
+            return -1;
+        }
+        list.add(recursiveInt, target);
+        return recursiveInt;
     }
 
     public static int recursiveRemove(List<String> list, String target) {
-        return -1;
+        if (!list.isEmpty()) {
+            if (list.get(0).equals(target)) {
+                list.remove(0);
+                return recursiveRemove(list, target) + 1;
+            }
+            return recursiveRemove(list.subList(1, list.size()), target);
+        }
+        return 0;
     }
 
     public static int recursiveSum(List<Integer> list) {
-        return -1;
+        if (list.isEmpty()) {
+            return 0;
+        }
+        return list.get(0) +  recursiveSum(list.subList(1, list.size()));
     }
 
     public static int recursiveFactorial(int n) {
-        return -1;
+        if (n == 0) {
+            return 1;
+        }
+        return n * recursiveFactorial(n - 1);
     }
 
     public static int recursivePow(int base, int exponent) {
-        return -1;
+        if (exponent == 0) {
+            return 1;
+        }
+        return base * recursivePow(base, exponent - 1);
     }
 
     public static int recursiveFibonacci(int n) {
-        return -1;
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
     }
 
     public static boolean recursivePalindrome(String word) {
-        return false;
+        if (word.length() != 1) {
+            if (word.charAt(0) == word.charAt(word.length() - 1)) {
+                return  recursivePalindrome(word.substring(1, word.length() - 1));
+            }
+            return false;
+        }
+        return true;
     }
 
 
